eab814156c1566f7aa315173d39821d05fac1a59
Juan Gabriel Jimenez Campos <juangabriel.jimenez@unit4.com>
2018-03-21 14:39:42 +0100

    Use min versions

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap-reboot.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery/dist/jquery.js';
-import 'popper.js/dist/popper.js';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
