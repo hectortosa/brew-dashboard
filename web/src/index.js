e7c55c5fe8a0db3819f0eca92498d5910c5628e8
Juan Gabriel Jimenez Campos <juangabriel.jimenez@unit4.com>
2018-03-21 14:43:36 +0100

    Fix popper compilation

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
-import 'popper.js/dist/popper.min.js';
+import 'popper.js/dist/umd/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import App from './App';
