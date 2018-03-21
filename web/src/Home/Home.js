fbddd824ea188b06a2822357955c7fc411730d34
Juan Gabriel Jimenez Campos <juangabriel.jimenez@unit4.com>
2018-03-21 11:28:34 +0100

    Subscribe to measures

--- web/src/Home/Home.js
+++ web/src/Home/Home.js
@@ -13,12 +13,25 @@ class Home extends Component {
     constructor(props) {
         super(props);
 
+        console.log(props.measures)
+
         setTimeout(() => {
             const measures = this.props.measures || [];
             measures.forEach(x => this.tempSubject.next({
                 x: moment(x.timestamp, 'DD-MM-YYYYTHH:mm:ss').toDate(),
                 y: x.value
             }));
+        });
+    }
+
+    componentWillMount() {
+        this.props.subscribeToNewMeasures({
+            onNewItem: (item) => {
+                this.tempSubject.next({
+                    x: moment(item.timestamp, 'DD-MM-YYYYTHH:mm:ss').toDate(),
+                    y: item.value
+                });
+            }
         });
     }
 
@@ -31,14 +44,27 @@ class Home extends Component {
     }
 }
 
+const deviceId = 'raspberry-pi-temp-sb118';
+
 const Measures = gql`
     query {
-        listMeasuresByDevice(deviceId: "raspberry-pi-temp-sb118") {
+        listMeasuresByDevice(deviceId: "${deviceId}") {
             items {
                 timestamp
                 value
                 units
             }
+        }
+    }
+`;
+
+const NewMeasuresSubscription = gql`
+    subscription {
+        subscribeToNewMeasures(deviceId: "${deviceId}") {
+            measureId
+            timestamp
+            value
+            units
         }
     }
 `;
@@ -49,6 +75,28 @@ const HomeWithData = graphql(Measures, {
     },
     props: (props) => ({
         measures: props.data.listMeasuresByDevice && props.data.listMeasuresByDevice.items,
+        subscribeToNewMeasures: params => {
+            props.data.subscribeToMore({
+                document: NewMeasuresSubscription,
+                updateQuery: (prev, {subscriptionData}) => {
+                    if (!subscriptionData.data) {
+                        return prev;
+                    }
+
+                    const newItem = subscriptionData.data.subscribeToNewMeasures;
+
+                    if (params.onNewItem) {
+                        params.onNewItem(newItem);
+                    }
+
+                    return Object.assign({}, prev, {
+                        listMeasuresByDevice: {
+                            items: [newItem, ...prev.listMeasuresByDevice.items]
+                        }
+                    });
+                }
+            });
+        },
     })
 
 })(Home);
