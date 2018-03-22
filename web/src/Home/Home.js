8c5422816d71effccc7a1d4cf379f3874b0a88c7
Héctor Tortosa <hectortosa@icloud.com>
2018-03-22 09:43:57 +0100

    Replaced subscription with polling

--- web/src/Home/Home.js
+++ web/src/Home/Home.js
@@ -1,14 +1,23 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 import gql from 'graphql-tag';
-import {graphql} from 'react-apollo';
-import {Subject} from 'rxjs';
+import {
+    graphql
+} from 'react-apollo';
+import {
+    Subject
+} from 'rxjs';
 import * as moment from 'moment';
+import request from 'request-promise-native';
 
 import TemperatureChart from '../TemperatureChart';
 
+var tempSubjectGlobal = new Subject();
+
 class Home extends Component {
 
-    tempSubject = new Subject();
+    tempSubject = tempSubjectGlobal;
 
     constructor(props) {
         super(props);
@@ -20,6 +29,39 @@ class Home extends Component {
                 y: x.value
             }));
         });
+
+        setInterval(function () {
+                request({
+                    uri: 'https://zynbjtfvhncqvj4sfaxcbiqmdy.appsync-api.eu-west-1.amazonaws.com/graphql',
+                    method: 'POST',
+                    json: true,
+                    body: {
+                        "query": `query {
+                        listMeasuresByDevice(deviceId: "28-8000000117ab", limit: 1) {
+                              items {
+                                timestamp
+                                value
+                                units
+                              }
+                            }
+                          }`
+                    },
+                    headers: {
+                        "x-api-key": "da2-au2olyd6r5abfnurhqfze3bh6e"
+                    }
+                }).then(function (response) {
+                    var data = response.data.listMeasuresByDevice.items;
+                    console.log(`Data: ${JSON.stringify(data)}`);
+                    data.forEach(function(x) {
+                        tempSubjectGlobal.next({
+                            x: moment(x.timestamp, 'DD-MM-YYYYTHH:mm:ss').toDate(),
+                            y: x.value
+                        })
+                    });
+                }).catch(function (error) {
+                    console.log(`ERROR GETTING MEASURES: ${error}`);
+                });
+            }, 5000);
     }
 
     componentWillMount() {
@@ -34,9 +76,17 @@ class Home extends Component {
     }
 
     render() {
-        return (
-            <div className="text-center">
-                <TemperatureChart stream={this.tempSubject.asObservable()} width={200} height={500} label="Live data"/>
+        return ( <div className = "text-center" >
+            <TemperatureChart stream = {
+                this.tempSubject.asObservable()
+            }
+            width = {
+                200
+            }
+            height = {
+                500
+            }
+            label = "Live data" />
             </div>
         )
     }
@@ -44,7 +94,7 @@ class Home extends Component {
 
 const deviceId = '28-8000000117ab';
 
-const Measures = gql`
+const Measures = gql `
     query {
         listMeasuresByDevice(deviceId: "${deviceId}") {
             items {
@@ -56,7 +106,7 @@ const Measures = gql`
     }
 `;
 
-const NewMeasuresSubscription = gql`
+const NewMeasuresSubscription = gql `
     subscription {
         subscribeToNewMeasures(deviceId: "${deviceId}") {
             measureId
@@ -76,7 +126,9 @@ const HomeWithData = graphql(Measures, {
         subscribeToNewMeasures: params => {
             props.data.subscribeToMore({
                 document: NewMeasuresSubscription,
-                updateQuery: (prev, {subscriptionData}) => {
+                updateQuery: (prev, {
+                    subscriptionData
+                }) => {
                     if (!subscriptionData.data) {
                         return prev;
                     }
